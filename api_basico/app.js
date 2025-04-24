ae4f4184ad800988e203886ecf35d61d2e691a88
Mariano <a01029708@tec.mx>
2025-04-24 13:40:32 -0600

    Commit inicial api basico

---
+++ api_basico/app.js
@@ -0,0 +1,10 @@
+"use strict";
+
+import express from "express";
+
+const PORT = 3000;
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
