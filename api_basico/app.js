1a061652cd56acb8d22d98d5bbb37d4f66c7b2c6
Mariano <a01029708@tec.mx>
2025-04-24 14:10:32 -0600

    Structure for first commit@

--- api_basico/app.js
+++ api_basico/app.js
@@ -2,6 +2,31 @@
 
 import express from "express";
 
+class user {
+    constructor(id, name, mail, items){
+        this.id = id;
+        this.name = name;
+        this.mail = mail;
+        this.items = items;
+    }
+}
+
+class item {
+    constructor(id, name, type, effect){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+}
+
+//Catalog
+const items = [
+    new item(1, "Sword", "Weapon", "Sharpness"),
+    new item(2, "Shield", "Armor", "Defense"),
+    new item(3, "Potion", "Consumable", "Healing"),
+]
+
+
 const PORT = 3000;
 const app = express();
 app.use(express.json());
