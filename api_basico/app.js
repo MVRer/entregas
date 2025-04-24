826a4d478959cd79bf71bd203370407c06a6f692
Mariano <a01029708@tec.mx>
2025-04-24 14:51:56 -0600

    Added /api/items/add post request and /api/items

--- api_basico/app.js
+++ api_basico/app.js
@@ -1,6 +1,7 @@
 "use strict";
 
 import express from "express";
+import fs from "fs";
 
 class user {
     constructor(id, name, mail, items){
@@ -34,8 +35,60 @@ const users = [
 
 const PORT = 3000;
 const app = express();
+
 app.use(express.json());
-app.use(express.static("public"));
+
+app.use(express.static("./public"));
+app.get("/", (req, response) => {
+    fs.readFile("./public/html/index.html", "utf8", (err,data) =>{
+        if (err) {
+            response.status(500).send("Error reading file");
+            return;
+        }
+        response.send(data);
+    })
+})
+
+// ITEM API
+app.get("/api/items", (req, res) => {
+    res.json(items);
+    
+});
+
+
+app.post("/api/items/add/", (req, res) => {
+    const item_tmp = req.body;
+    
+    if (!item_tmp.name || !item_tmp.type || !item_tmp.effect){
+        res.status(400).json({ message: "Wrong format",
+            format: "json",
+            example: {
+                name: "Sword",
+                type: "Weapon",
+                effect: "Sharpness"
+            }
+        });
+        return;
+    }
+    for (let i = 0; i < items.length; i++) {
+        if (items[i].name === item_tmp.name) {
+            res.status(400).json({ message: "Item already exists" });
+            return;
+        }
+    }
+    
+
+    //const item = new item(item.id, item_tmp.name, item_tmp.type, item_tmp.effect);
+    //items.push(item);
+    const newItem = new item(items.length + 1, item_tmp.name, item_tmp.type, item_tmp.effect);
+    items.push(newItem);
+    res.status(200).json({ message: "Item added successfully" });
+    return;
+    
+}) 
+
+
+
 app.get("/api/hello", (req, res) => {
     res.json({ message: "Hello from the server!" });
 });
