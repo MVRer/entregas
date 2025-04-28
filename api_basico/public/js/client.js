bf2242e781d72e96a2ae9feb81d9911e8f4e2e7d
Mariano <a01029708@tec.mx>
2025-04-28 13:15:09 -0600

    Syntax fix for better work

--- api_basico/public/js/client.js
+++ api_basico/public/js/client.js
@@ -1,6 +1,6 @@
 async function main() {
-    const response = await fetch("http://localhost:3333/api/hello");
-    const message = await response.json();
+    let response = await fetch("http://localhost:3000/api/hello");
+    let message = await response.json();
     console.log(message);
 }
 main();
