2c0b9e8637c11bd7326c55bbc66e7ed92bedc533
Mariano <a01029708@tec.mx>
2025-04-28 13:55:18 -0600

    Added endpoint /api/users/add/ POST

--- api_basico/app.js
+++ api_basico/app.js
@@ -28,8 +28,8 @@ const items = [
     new item(3, "Potion", "Consumable", "Healing"),
 ]
 const users = [
-    new user(1, "John Doe", "jhon@gmail.com", [items[0], items[1]]),
-    new user(2, "Jane Smith", "jane@gmail.com", [items[1], items[2]]),
+    new user(1, "John Doe", "jhon@gmail.com", [1,2]),
+    new user(2, "Jane Smith", "jane@gmail.com", [3]),
 ]
 
 
@@ -40,6 +40,65 @@ const app = express();
 
 app.use(express.json());
 app.use(express.static("./public"));
+
+app.post("/api/users/add/", (req, res) => {
+    const users_tmp = req.body;
+    //console.log(users_tmp);
+
+    if (!Array.isArray(users_tmp)) {
+        res.status(400).json({ message: "Wrong format, must be a list of users from length 1 to infinity",
+            format: "json",
+            example: {
+                name: "Jhon",
+                mail: "doe@gmail.com",
+                items: "[1,2]"
+            }
+        });
+        return;
+    }
+
+    for (let i = 0; i < users_tmp.length; i++) {
+        const user_tmp = users_tmp[i];
+        //console.log(user_tmp);
+        if (!user_tmp.name || !user_tmp.mail || !user_tmp.items){
+            res.status(400).json({ message: "Wrong format",
+                format: "json",
+                example: {
+                    name: "Jhon",
+                    mail: "doe@gmail.com",
+                    items: "[1,2]"
+                },
+                error_in: "user: " + user_tmp.name
+            });
+            return;
+        }
+        for (let j = 0; j < users.length; j++) {
+            if (users[j].mail === user_tmp.mail) {
+                res.status(400).json({ message: "User with that mail already exists", error: "Error in user: " + user_tmp.name });
+                return;
+            }
+        }
+        for (let j = 0; j < user_tmp.items.length; j++) {
+            const item_tmp = user_tmp.items[j];
+            let found = false;
+            for (let k = 0; k < items.length; k++) {
+                if (items[k].id === item_tmp) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                res.status(400).json({ message: "Item with that id does not exist", error: "Error in user: " + user_tmp.name + " item: " + item_tmp });
+                return;
+            }
+        }
+        const newuser = new user(users.length + 1, user_tmp.name, user_tmp.mail, user_tmp.items);
+        users.push(newuser);
+    }
+    res.status(200).json({ message: "Users added successfully" });
+    return;
+    
+});
 app.get("/", (req, response) => {
     fs.readFile("./public/html/index.html", "utf8", (err,data) =>{
         if (err) {
--- api_basico/public/js/client.js
+++ api_basico/public/js/client.js
@@ -2,5 +2,28 @@ async function main() {
     let response = await fetch("http://localhost:3000/api/hello");
     let message = await response.json();
     console.log(message);
+    const users = [
+        {
+            name: "Jhon",
+            mail: "didi@gmail.com",
+            items: [1, 2]
+        },
+        {
+            name: "Doe",
+            mail: "cocolazo@gmail.com",
+            items: [1, 3]
+        }
+    ]
+
+
+    response = await fetch("http://localhost:3000/api/users/add", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(users)
+    });
+    message = await response.json();
+    console.log(message);
 }
 main();
