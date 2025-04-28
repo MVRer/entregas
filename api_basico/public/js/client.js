292e1bb1b4f8cefecd51e7f671bc57605361c856
Mariano <a01029708@tec.mx>
2025-04-28 13:06:16 -0600

    Added test

--- api_basico/public/js/client.js
+++ api_basico/public/js/client.js
@@ -1,6 +1,9 @@
 async function main() {
-    const response = await fetch("http://localhost:3333/api/hello");
-    const message = await response.json();
+    let response = await fetch("http://localhost:3000/api/hello");
+    let message = await response.json();
+    console.log(message);
+    response = await fetch("http://localhost:3000/api/items/find/1");
+    message = await response.json();
     console.log(message);
 }
 main();
