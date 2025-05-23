192e7381ab2e58821cd0ceb46dd8f0078b5411cc
MVRer <mariano@deltadinamica.com>
2025-05-23 11:45:09 -0600

    Fixed and added good things

---
+++ E1-syntax-highlighter/README.md
@@ -0,0 +1,32 @@
+# Programming Language Parser for Elixir in Elixir
+## Description
+
+This is a basic syntax highligher for Elixir. This project uses regex to find token types and outputs an html file colored according to the found tokens. 
+
+## Usage
+``` bash
+iex project.exs
+```
+``` elixir
+#Add the file name you want to analyze
+Filework.tokenidentifier("FILENAME")
+
+```
+
+
+## The process behind it
+It has a list of regex patterns with its corresponding atom to help in identifying each word as what it supossed to be.
+For example:
+- ` [~r/^:([\w@]+|"[^"]*")/, :atom]`
+
+1. It reads the file and streams it line by line, calling the private function tokenfinder function for each line. 
+2. Tokenfinder will start calling the private function  tokenfinderhelper passing the line and regex which will call itself until it identifies the regex expresion by running the whole list or regex through the line. 
+3. Tokenfinderhelper will then return the word found with the atom to the corresponding token type.
+4. Tokenfinder will split_at the line, by length found and add the token with its corresponding html tags using the atom to an acumulator, which will start forming the html file back again.
+5. The main tokenidentifier will create the file and output the html.
+
+
+## Files
+- project.exs
+- colors.css
+- README.md
---
+++ E1-syntax-highlighter/project.exs
@@ -0,0 +1,116 @@
+# Programming language parser for elixir
+# This is a very basic non-context syntax highlighter
+# Made by Mariano Carretero
+# A01029708
+# May 6, 2025
+
+
+defmodule Filework do
+  # This is a comment
+  @moduledoc "A basic syntax highlighter for Elixir code that generates HTML output. "
+
+  defp tokenfinderhelper([[pattern, type] | tail], string) do
+    find = Regex.run(pattern, string)
+
+    cond do
+      find == nil -> tokenfinderhelper(tail, string)
+      find != nil -> [find, type]
+    end
+  end
+
+  defp tokenfinderhelper([], string) do
+    [string, :notfound]
+  end
+
+  defp tokenfinder([], _token_patterns, acc) do
+    Enum.reverse(acc)
+  end
+
+  defp tokenfinder(line, token_patterns, acc) do
+    [found, type] = tokenfinderhelper(token_patterns, line)
+
+    found_str = if is_list(found), do: hd(found), else: found
+
+    fixed_str =
+      found_str
+      |> String.replace("&", "&amp;")
+      |> String.replace("<", "&lt;")
+      |> String.replace(">", "&gt;")
+      |> String.replace("\"", "&quot;")
+
+    found_length = String.length(found_str)
+
+    if found_length == 0 do
+      Enum.reverse(acc)
+    else
+      {_useless, splitted} = String.split_at(line, found_length)
+
+      tokenfinder(splitted, token_patterns, [
+        "<span class=\"#{type}\">" <> fixed_str <> "</span>" | acc
+      ])
+    end
+  end
+
+  def tokenidentifier(in_filepath) do
+    token_patterns = [
+      [~r/^~r(\/[^\/]*\/|\{[^\}]*\}|\[[^\]]*\]|\([^\)]*\)|"[^"]*"|'[^']*')/, :regex],
+      [~r/^\s+/, :whitespace],
+      [~r/^\b(0x[0-9a-fA-F]+|0o[0-7]+|0b[01]+|\d+\.\d+([eE][+-]?\d+)?|\d+)\b/, :number],
+      [~r/^"""[^"]*?/, :string],
+      [~r/^"([^"\\]|\\.)*"|^"""([\s\S]*?)"""/, :string],
+
+      [~r/^\b(do|else|catch|rescue|after)\s*:/, :keyword],
+
+      [~r/^(\w+)(?=\s*:)/, :object_key],
+      [
+        ~r/^\b(after|and|catch|cond|def|defp|defmodule|defprotocol|defimpl|defmacro|defmacrop|defdelegate|defexception|defstruct|defoverridable|do|else|end|fn|for|if|in|import|quote|raise|receive|rescue|require|reraise|super|throw|try|unless|unquote|use|when|with|not|or)\b/,
+        :reserved_word
+      ],
+      [~r/^\b([a-z_][a-zA-Z0-9_?!]*)(?=\s*\()/, :function],
+      [~r/^\b([a-z_][a-zA-Z0-9_?!]*)\b/, :variable],
+      [~r/^[\(\)\{\}\[\],;\.]/, :punctuation],
+      [~r/^\b(true|false|nil)\b/, :boolean],
+      [~r/^#.*$/m, :comment],
+      [~r/^(&|\+|-|\*|\/|==|!=|<=|>=|<|>|=|&&|\|\||!|\+\+|--|<>|\|>|~>|\||in|\^)/, :operator],
+      [~r/^@(moduledoc|doc)\s+("""[\s\S]*?"""|"[^"]*")/, :docstring],
+      [~r/^@([a-zA-Z_][a-zA-Z0-9_]*)/, :special],
+      [
+        ~r/^\b(Integer|Float|String|Atom|List|Map|Tuple|Boolean|Function|PID|Port|Reference)\b/,
+        :data_type
+      ],
+      [~r/^@([A-Z][A-Z0-9_]*)\b/, :constant],
+      [~r/^\b(import|require|use|alias)\s+([A-Z][A-Za-z0-9_\.]*)/, :library],
+      [
+        ~r/^~[a-z](?:"""[\s\S]*?"""|"[^"]*"|'[^']*'|\([\s\S]*?\)|\[[\s\S]*?\]|\{[\s\S]*?\}|\/[^\/]*\/)/,
+        :sigil
+      ],
+
+      [~r/^\b([A-Z][A-Za-z0-9_]*)(\.[A-Z][A-Za-z0-9_]*)*\b/, :module],
+      [~r/^:([\w@]+|"[^"]*")/, :atom]
+    ]
+
+    html_content =
+      in_filepath
+      |> File.stream!()
+      |> Enum.map(&tokenfinder(&1, token_patterns, []))
+      |> Enum.map(&Enum.join(&1, ""))
+      |> Enum.join("")
+
+    html = """
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="colors.css">
+        <title>Document</title>
+      </head>
+      <body>
+        <pre>#{html_content}</pre>
+      </body>
+    </html>
+    """
+
+    File.write("out_file.html", html)
+  end
+end
